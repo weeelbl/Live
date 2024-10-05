e4f8b5f622726a95cb1daa9384dde3c0bfc85a80
Weeelb Lee <liwo8owil@gmail.com>
2024-10-06 01:01:00 +0900

    update contact

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,20 +1,10 @@
-// This script requires jQuery and jquery-form plugin
-// You can use these ones from Cloudflare CDN:
-// <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-// <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js" integrity="sha256-2Pjr1OlpZMY6qesJM68t2v39t+lMLvxwpa8QlRjJroA=" crossorigin="anonymous"></script>
-//
-$('#bootstrapForm').submit(function (event) {
-    event.preventDefault()
-    var extraData = {}
-    $('#bootstrapForm').ajaxSubmit({
-        data: extraData,
-        dataType: 'jsonp',  // This won't really work. It's just to use a GET instead of a POST to allow cookies from different domain.
-        error: function () {
-            // Submit of form should be successful but JSONP callback will fail because Google Forms
-            // does not support it, so this is handled as a failure.
-            alert('Form Submitted. Thanks.')
-            // You can also redirect the user to a custom thank-you page:
-            // window.location = 'http://www.mydomain.com/thankyoupage.html'
-        }
-    })
-})
+
+function handleSubmit() {
+    // 提交表单到 Google 表单
+    document.getElementById('bootstrapForm').submit();
+
+    // 提交后跳转到自定义页面
+    setTimeout(function() {
+        window.location.href = './thankyou/'; // 自定义感谢页面的 URL
+    }, 500); // 可根据需要调整延时
+}
